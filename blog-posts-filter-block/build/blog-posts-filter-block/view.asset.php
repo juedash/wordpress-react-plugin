ef5b3e12a0647567a9ef1a8698c18b57e9cb373a
juedash <juedasherifi@hotmail.com>
2026-04-03 18:36:46 +0200

    remove build files

--- blog-posts-filter-block/build/blog-posts-filter-block/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-html-entities'), 'version' => 'd581f2db20d92404b130');
--- blog-related-posts/build/blog-related-posts/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element'), 'version' => '60ae5cebc43180c37c4d');
